829c896c1e261fea8fa359567902c9e805975b9f
Chris Storch <ccstorch@gmail.com>
2018-01-22 19:09:17 +0000

    Update

--- validate.go
+++ validate.go
@@ -12,4 +12,18 @@ func (v *Validations) Add(field string, message string) error {
 	return errors.New("Invalid Record")
 }
 
+func (v *Validations) AddMany(field string, msgs []string) error {
+	if (*v)[field] == nil {
+		(*v)[field] = []string{}
+	}
+	(*v)[field] = append((*v)[field], msgs...)
+	return errors.New("Invalid Record")
+}
+
+func (v *Validations) MergeSubFieldValidations(v2 Validations, p string) {
+	for field, msgs := range v2 {
+		v.AddMany(p+"."+field, msgs)
+	}
+}
+
 // TODO: Handle SQL response errors
